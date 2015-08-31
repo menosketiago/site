1c1564438e95d034e833a60420e582f5d3725b31
Tiago Almeida <tiago@gooengine.com>
2015-08-31 10:07:10 +0200

    Partials not working yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	handlebars = require('gulp-compile-handlebars'),
+	rename = require('gulp-rename');
 
 var path = {
 	'bower': './bower_components',
-	'assets': './assets',
+	'node': './node_modules',
+	'assets': './source',
+	'templates': './source/templates'
 };
 
 gulp.task('styles', function() {
@@ -17,26 +21,39 @@ gulp.task('styles', function() {
 		]
 	}))
 	.pipe(concat('app.css'))
-	.pipe(gulp.dest('./public/css'));
+	.pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('scripts', function() {
 	gulp.src([
 		path.bower + '/jquery/dist/jquery.js',
-		path.bower + '/foundation/js/foundation.js',
-		path.bower + '/foundation/js/foundation/foundation.alert.js',
+		path.bower + '/foundation/js/foundation.min.js',
 		path.assets + '/scripts/app.js'
 	])
 	.pipe(concat('app.js'))
-	.pipe(gulp.dest('./public/js'));
+	.pipe(gulp.dest('./build/js'));
 
 	return gulp.src(path.bower + '/modernizr/modernizr.js')
-		.pipe(gulp.dest('./public/js'));
+		.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('handlebars', function() {
+	var options = {
+		batch : [path.templates + '/partials']
+	}
+
+	gulp.src(path.templates + '/*.hbs')
+		.pipe(handlebars(options))
+		.pipe(rename(function(path) {
+			path.extname = '.html';
+		}))
+		.pipe(gulp.dest('./build'));
 });
 
 gulp.task('watch', function() {
 	gulp.watch(path.assets + '/styles/**/*.scss', ['styles']);
 	gulp.watch(path.assets + '/scripts/**/*.js', ['scripts']);
+	gulp.watch(path.templates + '/**/*.hbs', ['handlebars']);
 });
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['styles', 'scripts', 'handlebars']);
