5254f8cacf9eb1d2643578091d7b75372378169b
Tiago Almeida <menosketiago@gmail.com>
2017-01-21 17:21:19 +0100

    Small tweaks

--- www/js/index.js
+++ www/js/index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 // Paralax on hero section
 $('#hero-bg').mousemove(function(e){
-    var ammountX = (e.pageX * -0.05);
-    var ammountY = (e.pageY * -0.05);
+    var ammountX = (e.pageX * -0.06);
+    var ammountY = (e.pageY * -0.06);
     $(this).css('background-position', ammountX + 'px ' + ammountY + 'px');
 });
 
