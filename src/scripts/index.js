1289f2e0b22b376349622113b017e4ddfa3d17f9
Tiago Almeida <menosketiago@gmail.com>
2017-01-21 16:53:16 +0100

    Added icon font, some more styling and improved the hero parallax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
 // Paralax on hero section
 $('#hero-bg').mousemove(function(e){
-    var amountMovedX = (e.pageX * -0.05);
-    var amountMovedY = (e.pageY * -0.05);
-    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+    var ammountX = (e.pageX * -0.05);
+    var ammountY = (e.pageY * -0.05);
+    $(this).css('background-position', ammountX + 'px ' + ammountY + 'px');
 });
 
 });
