f3538b0b78a850fe76408632bf81006483025aaf
Tiago Almeida <menosketiago@gmail.com>
2021-07-01 23:08:00 +0200

    Added some mobile styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,8 @@
 // IMPORTS
 
 import {initComponents} from './components/_init';
-import {checkWebPSupport} from './components/_webp';
 
 // INIT JS
 
 window.initComponents = initComponents;
 window.modalContentFetched = false;
-
-window.addEventListener('load', () => {
-    window.initComponents();
-
-    checkWebPSupport(function(support) {
-        if (!support) document.querySelector('html').classList.add('no-webp');
-    });
-});
